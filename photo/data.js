3769e1a55c2c44a25b049127e0828fada3f5d600
Maxwell <3202725220@qq.com>
2020-06-10 19:42:54 +0800

    romantic and interesting

---
+++ photo/data.js
@@ -0,0 +1,113 @@
+var data = [{
+		"img":"1.jpg",
+		"title":"第一张",
+		"desc":""
+	}, {
+		"img":"2.jpg",
+		"title":"第二张",
+		"desc":""
+	},{
+		"img":"3.jpg",
+		"title":"第三张",
+		"desc":""
+	},{
+		"img":"4.jpg",
+		"title":"第四张",
+		"desc":""
+	}, {
+		"img":"5.jpg",
+		"title":"第五张",
+		"desc":""
+	}, {
+		"img":"6.jpg",
+		"title":"第六张",
+		"desc":""
+	}, {
+		"img":"7.jpg",
+		"title":"第七张",
+		"desc":""
+	}, {
+		"img":"8.jpg",
+		"title":"第八张",
+		"desc":""
+	}, {
+		"img":"9.jpg",
+		"title":"第九张",
+		"desc":""
+	}, {
+		"img":"10.jpg",
+		"title":"第十张",
+		"desc":""
+	}, {
+		"img":"11.jpg",
+		"title":"第十一张",
+		"desc":""
+	}, {
+		"img":"12.jpg",
+		"title":"第十二张",
+		"desc":""
+	}
+	, {
+		"img":"13.jpg",
+		"title":"第十三张",
+		"desc":""
+	}, {
+		"img":"14.jpg",
+		"title":"第十四张",
+		"desc":""
+	}, {
+		"img":"15.jpg",
+		"title":"第十五张",
+		"desc":""
+	}, {
+		"img":"16.jpg",
+		"title":"第十六张",
+		"desc":""
+	}, {
+		"img":"17.jpg",
+		"title":"第十七张",
+		"desc":""
+	}, {
+		"img":"18.jpg",
+		"title":"第十八张",
+		"desc":""
+	}, {
+		"img":"19.jpg",
+		"title":"第十九张",
+		"desc":""
+	}, {
+		"img":"20.jpg",
+		"title":"第二十张",
+		"desc":""
+	}
+	, {
+		"img":"21.jpg",
+		"title":"第二十一张",
+		"desc":""
+	},
+	{
+		"img":"22.jpg",
+		"title":"第二十二张",
+		"desc":""
+	},{
+		"img":"23.jpg",
+		"title":"第二十三张",
+		"desc":""
+	}, {
+		"img":"24.jpg",
+		"title":"第二十四张",
+		"desc":""
+	}, {
+		"img":"25.jpg",
+		"title":"第二十五张",
+		"desc":""
+	}, {
+		"img":"26.jpg",
+		"title":"第二十六张",
+		"desc":""
+	}
+	];  
+
+var obj1 = eval(data);  
+console.log(data);
+
